491573725ad70ab214a2c3ed2c2e9173e8564501
Julien Marrec <julien.marrec@gmail.com>
2022-01-06 13:46:39 +0100

    Bump boost from 1.73.0 to 1.78.0 for MSVC 2022 support

--- ConanInstall.cmake
+++ ConanInstall.cmake
@@ -81,7 +81,7 @@ if(NOT CONAN_OPENSTUDIO_ALREADY_RUN)
     ${CONAN_QT}
     ${CONAN_RUBY}
     "openssl/1.1.0l#7f3fa5cfcfba31fffa344c71a9795176" # ruby 2.5.5 won't work with 1.1.1x, so use 1.1.0l here to try to force every package to align on the same as ruby
-    "boost/1.73.0#4129a76c9b83c300fc103e36d1908792"
+    "boost/1.78.0#c6275b1e5ecf6e51fe8baf1e322bb065"
     "pugixml/1.10#64b3ebc897bb9d9854c8a2443bf112a8"
     "jsoncpp/1.9.3#073a6d3cb40911d7c8027bddb6ae7dbf"
     "zlib/1.2.11#0df31fd24179543f5720ec7beb2a88d7"
